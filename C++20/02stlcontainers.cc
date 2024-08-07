12672af68962c21ab57e136c4b1878b709d9c2dd
HuaTsai <huatsai.eed07g@nctu.edu.tw>
2024-08-07 23:13:28 +0800

    update: 2024-08-07

--- C++20/02stlcontainers.cc
+++ C++20/02stlcontainers.cc
@@ -14,6 +14,82 @@ void print_assoc(auto& r) {
   cout << "\n";
 }
 
+struct Point {
+  int x, y;
+  Point(int x, int y) : x(x), y(y) {}
+  bool operator==(const Point& p) const { return x == p.x && y == p.y; }
+};
+
+template <>
+struct std::hash<Point> {
+  size_t operator()(const Point& p) const {
+    return hash<int>()(p.x) ^ hash<int>()(p.y);
+  }
+};
+
+class RPN {
+  deque<double> deq_{};
+  constexpr static double zero_{0.0};
+  constexpr static double inf_{std::numeric_limits<double>::infinity()};
+
+ public:
+  double op(const string& s) {
+    if (is_numeric(s)) {
+      double v{stod(s, nullptr)};
+      deq_.push_front(v);
+      return v;
+    } else {
+      return optor(s);
+    }
+  }
+
+  void clear() { deq_.clear(); }
+
+  string get_stack_string() const {
+    string s{};
+    for (auto v : deq_) {
+      s += format("{} ", v);
+    }
+    return s;
+  }
+
+ private:
+  pair<double, double> pop_get2() {
+    if (deq_.size() < 2) return {zero_, zero_};
+    double v1{deq_.front()};
+    deq_.pop_front();
+    double v2{deq_.front()};
+    deq_.pop_front();
+    return {v2, v1};
+  }
+
+  bool is_numeric(const string& s) {
+    for (const char c : s) {
+      if (c != '.' && !std::isdigit(c)) return false;
+    }
+    return true;
+  }
+
+  double optor(const string& op) {
+    map<string, double (*)(double, double)> opmap{
+        {"+", [](double l, double r) { return l + r; }},
+        {"-", [](double l, double r) { return l - r; }},
+        {"*", [](double l, double r) { return l * r; }},
+        {"/", [](double l, double r) { return l / r; }},
+        {"^", [](double l, double r) { return pow(l, r); }},
+        {"%", [](double l, double r) { return fmod(l, r); }}};
+
+    if (opmap.find(op) == opmap.end()) return zero_;
+    auto [l, r] = pop_get2();
+
+    if (op == "/" && r == zero_)
+      deq_.push_front(inf_);
+    else
+      deq_.push_front(opmap.at(op)(l, r));
+    return deq_.front();
+  }
+};
+
 int main() {
   // Erase-remove idiom -> directly remove in C++20
   vector<int> c = {1, 2, 3, 4, 5};
@@ -124,4 +200,70 @@ int main() {
     cout << "m1.begin() is extracted and then inserted\n";
   }
 
+  // custom key of unordered_map
+  // unordered_map<Point, int>  -> use hash specialization, more general
+  // unordered_map<Point, int, hash_func_type> -> use hash function
+  unordered_map<Point, int> ump { {{0, 0}, 1}, {{1, 1}, 2}, {{2, 2}, 3} };
+  for (const auto &[k, v] : ump) {
+    cout << format("{{ ({}, {}), {} }} ", k.x, k.y, v);
+  }
+  cout << endl;
+
+  // Inserter for set
+  set<string> wd;
+  cout << "Enter words: ";
+  istream_iterator<string> isit(cin);
+  istream_iterator<string> end;  // default eof
+  copy(isit, end, inserter(wd, wd.end()));
+  cout << "Words: " << wd.size() << endl;
+  for (auto &w : wd) cout << w << " ";
+  cout << endl;
+
+  // RPN Calculator
+  RPN rpn;
+  vector<string> opv{"9", "6", "*", "2", "3", "*", "+"};
+  for(auto o : opv) {
+    rpn.op(o);
+    auto stack_str{rpn.get_stack_string()};
+    cout << format("{}: {}\n", o, stack_str);
+  }
+
+  // Word frequency
+  map<string, int> wordmap{};
+  vector<pair<string, int>> wordvec{};
+  regex word_re(R"((\w+))");  // R"delim ... delim", (): capture group
+
+  ifstream ifs("input.txt");
+  size_t total_words{};
+  for (string s{}; ifs >> s; ) {  // may be better than while loop, which s is declared outside
+    sregex_iterator words_begin(s.begin(), s.end(), word_re);
+    sregex_iterator words_end;
+    for(auto r_it{words_begin}; r_it != words_end; ++r_it) {
+      smatch match{*r_it};
+      auto word_str{match.str()};
+      ranges::transform(word_str, word_str.begin(), [](unsigned char c) { return tolower(c); });
+      // Important:
+      //   old method -> check if the key exists, if not insert 1, else increment
+      //   new method -> try_emplace, get reference and increment
+      auto [map_it, result] = wordmap.try_emplace(word_str, 0);
+      auto &[w, count] = *map_it;
+      ++total_words;
+      ++count;
+    }
+  }
+
+  auto unique_words = wordmap.size();
+  wordvec.reserve(unique_words);
+  ranges::move(wordmap, back_inserter(wordvec));
+  ranges::sort(wordvec, [](const auto& a, const auto& b) { 
+    return (a.second != b.second) ? (a.second > b.second) : (a.first < b.first);
+  });
+
+  cout << format("total word count: {}\n", total_words);
+  cout << format("unique word count: {}\n", unique_words);
+
+  for(int limit{20}; auto& [w, count] : wordvec) {
+    cout << format("{}: {}\n", count, w);
+    if(--limit == 0) break;
+  }
 }
